2f939d4ab6bdb4dcbe74cf64e576fe6f90e43279
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-02-06 15:56:56 +0000

    Fixed treehouses to have only 1 layer of ground on the bottom. Also disabled crystallite cluster debug text

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteClusterGeneratedProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteClusterGeneratedProcedure.java
@@ -5,7 +5,7 @@ import net.minecraft.network.chat.Component;
 
 public class CrystalliteClusterGeneratedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (true) {
+		if (false) {
 			if (!world.isClientSide() && world.getServer() != null)
 				world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("Crystallite cluster at: " + "X = " + x + ", Y = " + y + ", Z = " + z)), false);
 		}
