350675f2edda32c14961604ffe2c516d1e67c578
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-03-22 20:16:29 +0000

    Re-instated Sweeping Edge IV weapons replacing lower levels of the enchantment

--- src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteGoldSweepingEdgeProcedure.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/procedures/CrystalliteGoldSweepingEdgeProcedure.java
@@ -10,7 +10,7 @@ public class CrystalliteGoldSweepingEdgeProcedure {
 	public static void execute(Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		if (!(EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SWEEPING_EDGE, itemstack) != 0)) {
+		if (!(EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SWEEPING_EDGE, itemstack) != 0) || itemstack.getEnchantmentLevel(Enchantments.SWEEPING_EDGE) < 4) {
 			itemstack.enchant(Enchantments.SWEEPING_EDGE, 4);
 			if (entity instanceof Player _player)
 				_player.giveExperiencePoints(-(22));
