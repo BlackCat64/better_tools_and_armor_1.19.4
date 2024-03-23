7a48b898469241990a78e59927a8c9067574f862
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-03-23 21:33:09 +0000

    Added JEI Info for utility potions

--- src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModJeiInformation.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModJeiInformation.java
@@ -156,5 +156,7 @@ public class BetterToolsModJeiInformation implements IModPlugin {
 		registration.addIngredientInfo(List.of(new ItemStack(BetterToolsModItems.RECALL_POTION.get())), VanillaTypes.ITEM_STACK, Component.translatable("jei.better_tools.recall_potion_info"));
 		registration.addIngredientInfo(List.of(new ItemStack(BetterToolsModItems.LOST_SOULS_POTION.get())), VanillaTypes.ITEM_STACK, Component.translatable("jei.better_tools.lost_souls_potion_info"));
 		registration.addIngredientInfo(List.of(new ItemStack(BetterToolsModBlocks.JEWELLERY_TABLE.get())), VanillaTypes.ITEM_STACK, Component.translatable("jei.better_tools.jewellery_table_info"));
+		registration.addIngredientInfo(List.of(new ItemStack(BetterToolsModItems.EXPERIENCE_POTION.get()), new ItemStack(BetterToolsModItems.ASCENSION_POTION.get()), new ItemStack(BetterToolsModItems.DESCENSION_POTION.get()),
+				new ItemStack(BetterToolsModItems.RECALL_POTION.get()), new ItemStack(BetterToolsModItems.LOST_SOULS_POTION.get())), VanillaTypes.ITEM_STACK, Component.translatable("jei.better_tools.utility_potions_info"));
 	}
 }
