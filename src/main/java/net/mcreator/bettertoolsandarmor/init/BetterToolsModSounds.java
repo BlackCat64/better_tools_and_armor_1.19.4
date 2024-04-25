0d683cf98dd8928d507ade39e61c6b4c947eb9c1
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-04-25 11:42:15 +0100

    Removed old sound files. Replaced Rick Roll Disc with Crystallite Charm Base in treehouse loot tables.

--- src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModSounds.java
+++ src/main/java/net/mcreator/bettertoolsandarmor/init/BetterToolsModSounds.java
@@ -15,13 +15,6 @@ import net.mcreator.bettertoolsandarmor.BetterToolsMod;
 
 public class BetterToolsModSounds {
 	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, BetterToolsMod.MODID);
-	public static final RegistryObject<SoundEvent> GLIDE = REGISTRY.register("glide", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "glide")));
-	public static final RegistryObject<SoundEvent> DOWN_UNDER = REGISTRY.register("down_under", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "down_under")));
-	public static final RegistryObject<SoundEvent> BLOCK_DANCE = REGISTRY.register("block_dance", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "block_dance")));
-	public static final RegistryObject<SoundEvent> MASTER_BUILDERS = REGISTRY.register("master_builders", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "master_builders")));
-	public static final RegistryObject<SoundEvent> MOGUS_DRIP = REGISTRY.register("mogus_drip", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "mogus_drip")));
-	public static final RegistryObject<SoundEvent> RICK_ROLL = REGISTRY.register("rick_roll", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "rick_roll")));
-	public static final RegistryObject<SoundEvent> SHRUNK_MUSIC = REGISTRY.register("shrunk_music", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "shrunk_music")));
 	public static final RegistryObject<SoundEvent> CRYSTALLITE_BREAK = REGISTRY.register("crystallite_break", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "crystallite_break")));
 	public static final RegistryObject<SoundEvent> CRYSTALLITE_PLACE = REGISTRY.register("crystallite_place", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "crystallite_place")));
 	public static final RegistryObject<SoundEvent> CRYSTALLITE_STEP = REGISTRY.register("crystallite_step", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("better_tools", "crystallite_step")));
